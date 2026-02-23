27467ffc92474194430f53034ced20edab0b6174
HabilMuhammad14 <habilmuha14@gmail.com>
2026-02-23 13:09:44 +0700

    Delete New folder directory

--- New folder/routes/classes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import classesController from '../controllers/classesController.js'
-const router = express.Router()
-
-router.get('/:id', classesController.getAllClassesByID);
-router.get('/', classesController.getAllClassesForMhs)
-
-export default router;
