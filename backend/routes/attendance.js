6cfcec8b6769baea3bc6e28f4bfcdd28206748cc
HabilMuhammad14 <habilmuha14@gmail.com>
2026-02-26 16:12:15 +0700

    Add createAttendance route to attendance.js

--- backend/routes/attendance.js
+++ backend/routes/attendance.js
@@ -4,5 +4,6 @@ const router = express.Router()
 
 router.put('/open/:id', attendanceController.openAbsent);
 router.put('/close/:id', attendanceController.closeAbsent);
+router.post('/attend', attendanceController.createAttendance);
 
 export default router
